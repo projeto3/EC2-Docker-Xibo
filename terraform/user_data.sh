88302cb973e077ca1b1feb2176c3867f76d0fddf
projeto3 <42242436+projeto3@users.noreply.github.com>
2018-10-29 20:50:52 -0300

    Update user_data.sh

--- terraform/user_data.sh
+++ terraform/user_data.sh
@@ -2,9 +2,24 @@
 
 #Libera acesso root
 sed -i '/disable_root: true/ s/true/false/g' /etc/cloud/cloud.cfg
-sed -i '/ssh_pwauth: false/ s/false/true/g' /etc/cloud/cloud.cfg
-
+sed -i '/ssh_pwauth:   false/ s/false/true/g' /etc/cloud/cloud.cfg
 sed -i '/PasswordAuthentication no/ s/no/yes/g' /etc/ssh/sshd_config
 sed -i '/PermitRootLogin no/ s/no/yes/g' /etc/ssh/sshd_config
+
+#Descomenta a linha
+sed -i '/disable_root:/ s/^#//' /etc/cloud/cloud.cfg
+sed -i '/ssh_pwauth:/ s/^#//' /etc/cloud/cloud.cfg
+sed -i '/PasswordAuthentication/ s/^#//' /etc/ssh/sshd_config
+sed -i '/PermitRootLogin/ s/^#//' /etc/ssh/sshd_config
+
+#Usuario de Serviço
+usermod --password PASSWORD root
+
+
+sudo adduser ansible
+usermod --password PASSWORD ansible
+
+
+
 sudo service sshd restart
 
