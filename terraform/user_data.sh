843dd40e32c4291f47a892d17a141353e82ed8fb
projeto3 <42242436+projeto3@users.noreply.github.com>
2018-10-29 19:54:40 -0300

    Update user_data.sh

--- terraform/user_data.sh
+++ terraform/user_data.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-yum -y update
-yum -y install epel-release
-wget http://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
-rpm -ivh epel-release-latest-7.noarch.rpm
-yum repolist
-yum -y --enablerepo=epel install ansible python-pip python-wheel
-curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-python get-pip.py
-pip install boto3
+
+#Libera acesso root
+sed -i '/disable_root: true/ s/true/false/g' /etc/cloud/cloud.cfg
+sed -i '/ssh_pwauth: false/ s/false/true/g' /etc/cloud/cloud.cfg
+
+sed -i '/PasswordAuthentication no/ s/no/yes/g' /etc/ssh/sshd_config
+sed -i '/PermitRootLogin no/ s/no/yes/g' /etc/ssh/sshd_config
+sudo service sshd restart
+
