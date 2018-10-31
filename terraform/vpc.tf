bec2ac495549590be2e951028b3a4c4a37b188ab
projeto3 <42242436+projeto3@users.noreply.github.com>
2018-10-31 18:46:40 -0300

    Update vpc.tf

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -14,7 +14,7 @@ resource "aws_subnet" "main-public-1" {
     vpc_id = "${aws_vpc.main.id}"
     cidr_block = "10.0.1.0/24"
     map_public_ip_on_launch = "true"
-    availability_zone = "us-west-1a"
+    availability_zone = "us-west-1b"
 
     tags {
         Name = "main-public-1"
@@ -26,7 +26,7 @@ resource "aws_subnet" "main-private-1" {
     vpc_id = "${aws_vpc.main.id}"
     cidr_block = "10.0.4.0/24"
     map_public_ip_on_launch = "false"
-    availability_zone = "us-west-1b"
+    availability_zone = "us-west-1c"
 
     tags {
         Name = "main-private-1"
