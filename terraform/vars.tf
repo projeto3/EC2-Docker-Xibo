59c78c6978100be29ba106b263460dff2a59a890
projeto3 <42242436+projeto3@users.noreply.github.com>
2018-10-31 19:47:47 -0300

    Update vars.tf

--- terraform/vars.tf
+++ terraform/vars.tf
@@ -1,4 +1,4 @@
-resource "aws_key_pair" "projeto3_ansible_xibo" {
-   # key_name = "projeto3_ansible_xibo"
-   # public_key = "${file("${path.module}/keys/id_rsa.pub")}"
-}
+variable "key_path" {
+  description = "SSH Public Key path"
+  default = "~/.ssh/id_rsa.pub"
+   }
