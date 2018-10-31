aa40d0273520e0ea93ac483703dce7c8791fd21d
projeto3 <42242436+projeto3@users.noreply.github.com>
2018-10-31 19:26:18 -0300

    Update vars.tf

--- terraform/vars.tf
+++ terraform/vars.tf
@@ -1,4 +1,6 @@
 variable "key_path" {
-  description = "SSH Public Key path"
-  default = "~/.ssh/id_rsa.pub"
+  resource "aws_key_pair" "projeto3_ansible_xibo" {
+  key_name = "projeto3_ansible_xibo"
+    public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAQEAmHKvQHsinpelrRcsc+1HKmjcVTx4LONzy+iXYDNoJppW52AKMvQs+x/YCb5qvtpAOgbXHOlAT5dUFqyzogY1Ch7cGEsXyoLHdZgMIsn1itwHAe0s87CjKvRKgjzBlLYTa+5b/Ai+mM3zJCvEbDIcSqvGFJIQeJHNOwj33R+8Z8Du7Np+hfT7HWx89B72fthoV+UcGkEmBbHe5CyHKT9p2ydSRqRkTisVAJGnNN4BooNG7iKcBxvKfal3PCLpqwvNME5OHiHPfBIT0rfrGupfifSYSlwzTG+lyFHEeBsbcDJfawhNNMhFpaKekt0BJVGf0wixm0MTrRNgcjg5FaaeDQ== rsa-key-20181029"  
+
 }
