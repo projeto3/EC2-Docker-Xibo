5f91ee0e9435f0a0b46c0d67cfa0b7b23acf5c8b
projeto3 <42242436+projeto3@users.noreply.github.com>
2018-10-31 19:52:42 -0300

    Update instance.tf

--- terraform/instance.tf
+++ terraform/instance.tf
@@ -7,7 +7,7 @@ resource "aws_instance" "projeto3" {
  
   connection {
         user = "ec2-user"
-        private_key = "ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAQEAmHKvQHsinpelrRcsc+1HKmjcVTx4LONzy+iXYDNoJppW52AKMvQs+x/YCb5qvtpAOgbXHOlAT5dUFqyzogY1Ch7cGEsXyoLHdZgMIsn1itwHAe0s87CjKvRKgjzBlLYTa+5b/Ai+mM3zJCvEbDIcSqvGFJIQeJHNOwj33R+8Z8Du7Np+hfT7HWx89B72fthoV+UcGkEmBbHe5CyHKT9p2ydSRqRkTisVAJGnNN4BooNG7iKcBxvKfal3PCLpqwvNME5OHiHPfBIT0rfrGupfifSYSlwzTG+lyFHEeBsbcDJfawhNNMhFpaKekt0BJVGf0wixm0MTrRNgcjg5FaaeDQ== rsa-key-20181029"
+        public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAQEAmHKvQHsinpelrRcsc+1HKmjcVTx4LONzy+iXYDNoJppW52AKMvQs+x/YCb5qvtpAOgbXHOlAT5dUFqyzogY1Ch7cGEsXyoLHdZgMIsn1itwHAe0s87CjKvRKgjzBlLYTa+5b/Ai+mM3zJCvEbDIcSqvGFJIQeJHNOwj33R+8Z8Du7Np+hfT7HWx89B72fthoV+UcGkEmBbHe5CyHKT9p2ydSRqRkTisVAJGnNN4BooNG7iKcBxvKfal3PCLpqwvNME5OHiHPfBIT0rfrGupfifSYSlwzTG+lyFHEeBsbcDJfawhNNMhFpaKekt0BJVGf0wixm0MTrRNgcjg5FaaeDQ== rsa-key-20181029"
     }
 
   #key_name = "${var.private_key_path)}"
