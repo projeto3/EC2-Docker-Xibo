957a9ebbf04a98a01170353bf5b76feea0dcd655
projeto3 <42242436+projeto3@users.noreply.github.com>
2018-10-31 19:22:36 -0300

    Update instance.tf

--- terraform/instance.tf
+++ terraform/instance.tf
@@ -4,7 +4,7 @@ resource "aws_instance" "projeto3" {
   instance_type = "t2.micro"
   #private_key = "${file("${path.module}/projeto3.pem")}"
   key_name = "projeto3_ansible_xibo"
-  private_key = "ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAQEAmHKvQHsinpelrRcsc+1HKmjcVTx4LONzy+iXYDNoJppW52AKMvQs+x/YCb5qvtpAOgbXHOlAT5dUFqyzogY1Ch7cGEsXyoLHdZgMIsn1itwHAe0s87CjKvRKgjzBlLYTa+5b/Ai+mM3zJCvEbDIcSqvGFJIQeJHNOwj33R+8Z8Du7Np+hfT7HWx89B72fthoV+UcGkEmBbHe5CyHKT9p2ydSRqRkTisVAJGnNN4BooNG7iKcBxvKfal3PCLpqwvNME5OHiHPfBIT0rfrGupfifSYSlwzTG+lyFHEeBsbcDJfawhNNMhFpaKekt0BJVGf0wixm0MTrRNgcjg5FaaeDQ== rsa-key-20181029"  
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAQEAmHKvQHsinpelrRcsc+1HKmjcVTx4LONzy+iXYDNoJppW52AKMvQs+x/YCb5qvtpAOgbXHOlAT5dUFqyzogY1Ch7cGEsXyoLHdZgMIsn1itwHAe0s87CjKvRKgjzBlLYTa+5b/Ai+mM3zJCvEbDIcSqvGFJIQeJHNOwj33R+8Z8Du7Np+hfT7HWx89B72fthoV+UcGkEmBbHe5CyHKT9p2ydSRqRkTisVAJGnNN4BooNG7iKcBxvKfal3PCLpqwvNME5OHiHPfBIT0rfrGupfifSYSlwzTG+lyFHEeBsbcDJfawhNNMhFpaKekt0BJVGf0wixm0MTrRNgcjg5FaaeDQ== rsa-key-20181029"  
   #key_name = "${var.private_key_path)}"
   #key_name = "${aws_key_pair.mykeypair.key_name}"
   subnet_id     = "${aws_subnet.main-public-1.id}"
