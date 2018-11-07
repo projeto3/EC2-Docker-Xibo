c46a71502682df574db33d8d1a3a6917f4ebd971
projeto3 <42242436+projeto3@users.noreply.github.com>
2018-11-07 19:09:41 -0300

    Update vpc.tf

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -21,15 +21,37 @@ resource "aws_subnet" "main-public-1" {
     }
 }
 
+resource "aws_subnet" "main-public-2" {
+    vpc_id = "${aws_vpc.main.id}"
+    cidr_block = "10.0.2.0/24"
+    map_public_ip_on_launch = "true"
+    availability_zone = "us-west-1c"
+
+    tags {
+        Name = "main-public-2"
+    }
+}
+
 
 resource "aws_subnet" "main-private-1" {
     vpc_id = "${aws_vpc.main.id}"
     cidr_block = "10.0.4.0/24"
     map_public_ip_on_launch = "false"
-    availability_zone = "us-west-1c"
+    availability_zone = "us-west-1"
 
     tags {
         Name = "main-private-1"
+    }
+}
+
+resource "aws_subnet" "main-private-2" {
+    vpc_id = "${aws_vpc.main.id}"
+    cidr_block = "10.0.5.0/24"
+    map_public_ip_on_launch = "false"
+    availability_zone = "us-west-1"
+
+    tags {
+        Name = "main-private-2"
     }
 }
 
@@ -59,5 +81,9 @@ resource "aws_route_table" "main-public" {
 # route associations public
 resource "aws_route_table_association" "main-public-1-a" {
     subnet_id = "${aws_subnet.main-public-1.id}"
+    route_table_id = "${aws_route_table.main-public.id}"
+}
+resource "aws_route_table_association" "main-public-2-a" {
+    subnet_id = "${aws_subnet.main-public-2.id}"
     route_table_id = "${aws_route_table.main-public.id}"
 }
