9afe23663f915a0e12307e9664ea8ba7b27f3b10
Marques Lima <marqueslima.dev@gmail.com>
2019-08-29 17:39:14 -0300

    key test

--- terraform/key.tf
+++ terraform/key.tf
@@ -1,3 +1,7 @@
 #resource "aws_key_pair" "default" {
 #  public_key = "${file("${var.key_path}")}"
 #}
+resource "aws_key_pair" "projeto-pipeline" {
+  key_name = "projeto-pipeline"
+  public_key = "tprojeto-pipeline.pub"
+}
