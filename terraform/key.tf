fa436e903d1d5fe32c9085d3458ec01c435b2f28
Marques Lima <marqueslima.dev@gmail.com>
2019-08-29 17:39:56 -0300

    remove

--- terraform/key.tf
+++ terraform/key.tf
@@ -1,7 +1,3 @@
 #resource "aws_key_pair" "default" {
 #  public_key = "${file("${var.key_path}")}"
 #}
-resource "aws_key_pair" "projeto-pipeline" {
-  key_name = "projeto-pipeline"
-  public_key = "tprojeto-pipeline.pub"
-}
